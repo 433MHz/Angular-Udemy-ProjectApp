8a24a31385fb72123b557e91056c9c3daf331d06
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-05-16 19:26:43 +0200

    Project initialized, header ready

---
+++ udemy-app/src/app/shoppingList/shopping-list-edit/shopping-list-edit.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector:'app-shopping-list-edit',
+    styleUrls:['./shopping-list-edit.component.css'],
+    templateUrl:'./shopping-list-edit.component.html'
+})
+
+export class ShoppingListEditComponent implements OnInit{
+
+    constructor(){
+
+    }
+
+    ngOnInit(): void{
+
+    }
+}
